3576815255a9394521057e8124a62431efe9c6f7
Brandon-Harris <brandonharris177@gmail.com>
2019-09-13 09:29:32 -0600

    index updated

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import {createStore, applyMiddleware, compose} from 'redux';
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+// import reducer from './reducers'
+
+const store = createStore(reducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, document.getElementById('root'));
