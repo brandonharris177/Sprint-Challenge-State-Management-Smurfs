ea1361b7e5764e0fe9c841c19657420dccada85c
Brandon-Harris <brandonharris177@gmail.com>
2019-09-13 10:08:49 -0600

    deleting and reuploading node modules

---
+++ smurfs/src/components/SmurfDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SmurfDisplay = () => {
+    
+    return (
+        <>
+        <div>Its working</div>
+        </>
+    )
+}
+
+export default SmurfDisplay
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -9,7 +9,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import {createStore, applyMiddleware, compose} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import reducer from './components/reducers'
